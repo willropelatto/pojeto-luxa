aef6874d76b97733051a51b86ed4772408b9bc0b
Willian.Ropelatto <willian.ropelatto@senior.com.br>
2017-04-25 12:36:21 -0300

    loucura do moa

--- ApiVcEhSafado/src/main/java/com/ctrl/game/PlayerController.java
+++ ApiVcEhSafado/src/main/java/com/ctrl/game/PlayerController.java
@@ -8,97 +8,94 @@ import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 
 import com.google.gson.Gson;
-import com.model.league.LeagueDAO;
-import com.model.league.LeagueEntity;
 import com.model.player.FullPlayer;
 import com.model.player.League;
 import com.model.player.Page;
-import com.model.player.PlayerEntity;
 import com.model.player.PlayerDAO;
-
-import java.util.ArrayList;
+import com.model.player.PlayerEntity;
+import com.model.player.ResumedPlayer;
 
 public class PlayerController {
-	
+
+
+	public ResumedPlayer convertPlayerFullToResumed(FullPlayer full) {
+
+		ResumedPlayer resumed = new ResumedPlayer();
+
+		resumed.setBaseId(full.getBaseId());
+		resumed.setId(full.getId());
+		resumed.setName(full.getName());
+		resumed.setPosition(full.getPosition());
+		resumed.setRating(full.getRating());	
+
+		return resumed;
+	}
+
+	public PlayerEntity convertPlayerFullToDB(FullPlayer fullpl) {
+
+		PlayerEntity player = new PlayerEntity();
+		Integer originalId = Integer.parseInt(fullpl.getId());
+		Integer idLeague = new Integer(fullpl.getLeague().getId());
+
+		player.setName(fullpl.getName());
+		player.setPosition(fullpl.getPosition());
+		player.setBaseId(fullpl.getBaseId());
+		player.setRating(fullpl.getRating());
+		player.setIdLeague(idLeague); 
+		player.setOriginalId(originalId);
+		return player;
+
+	}		
+
+
 	public void UpdateStoredPlayers() {
 		
-	
-		 Client client = ClientBuilder.newClient();
-		 WebTarget target = client.target("https://www.easports.com/fifa/ultimate-team/api/fut/item?page=1");						
-	    String json = target.request(MediaType.APPLICATION_JSON).get().readEntity(String.class);	    
-	    Gson gson = new Gson();	    
-	    Page pg = gson.fromJson(json, Page.class);	   
-	    
-	    ConverterPlayer conversorPlayer = new ConverterPlayer();
-	    ConverterLeague conversorLeague = new ConverterLeague();
-	    PlayerDAO playerDao = new PlayerDAO();
-	    
-	    PlayerEntity player;
-	    FullPlayer fullPlay;	  
-	    
-	    ArrayList<League> leagues = new ArrayList<League>();	    
+		Client client = ClientBuilder.newClient();
+		WebTarget target = client.target("https://www.easports.com/fifa/ultimate-team/api/fut/item?page=1");						
+		String json = target.request(MediaType.APPLICATION_JSON).get().readEntity(String.class);	    
+		Gson gson = new Gson();	    
+		Page pg = gson.fromJson(json, Page.class);	    
 
-	    while (pg.getTotalPages() >= pg.getPage()) {
-    
-	    	for (int i = 0; i < pg.getItems().length; i++) {
-	    		
-	    		fullPlay = pg.getItems()[i];    		
-	    		
-	    		if (fullPlay.getPlayerType().equals("rare") || 
-	    			fullPlay.getPlayerType().equals("standard")) {	    		
-	    			
-	    			League league = fullPlay.getLeague();
-	    			if (!leagues.contains(league)) {
-	    				leagues.add(league);
-	    				
-	    				LeagueDAO leagueDao = new LeagueDAO();
-	    				LeagueEntity leagueEn = conversorLeague.FullToDB(league);
-	    				leagueDao.Save(leagueEn);	
-	    			}
-	    			
-	    			player = conversorPlayer.FullToDB(fullPlay);
-	    			playerDao.Save(player);
+		PlayerDAO playerDao = new PlayerDAO();
 
-						
-	    		}	    		
-	    		
-	    	}
-	    	
-	    	int pageToGo = pg.getPage()+1;	   
-	    	
+		PlayerEntity player;
+		FullPlayer fullPlay;	  
+
+		ArrayList<League> leagues = new ArrayList<League>();	    
+
+		while (pg.getTotalPages() >= pg.getPage()) {
+
+			for (int i = 0; i < pg.getItems().length; i++) {
+
+				fullPlay = pg.getItems()[i];    		
+
+				if ((fullPlay.getPlayerType().equals("rare") || fullPlay.getPlayerType().equals("standard")) &&
+						!fullPlay.getColor().isEmpty())	{	    		
+
+					League league = fullPlay.getLeague();
+					if (!leagues.contains(league)) {
+						leagues.add(league);
+					}
+
+					player = convertPlayerFullToDB(fullPlay);
+					playerDao.Save(player);						
+				}	    		
+
+			}
+
+			int pageToGo = pg.getPage()+1;	   
+
 			if (pg.getPage() < pg.getTotalPages()) {	    	
 				target = client.target("https://www.easports.com/fifa/ultimate-team/api/fut/item?page="+pageToGo);
 				json = target.request(MediaType.APPLICATION_JSON).get().readEntity(String.class);
 				gson = new Gson();
 				pg = gson.fromJson(json, Page.class);	
 			}	    	
-	    	
-	    }	    
-	    
-	}
-	
-	public void UpdateTestMoacir() {
-		Client client = ClientBuilder.newClient();
-		WebTarget target = client.target("http://smartwaysolucoes.com/item7.json");
-	    String json = target.request(MediaType.APPLICATION_JSON).get().readEntity(String.class);
-	    Gson gson = new Gson();
-	    Page pg = gson.fromJson(json, Page.class);	   
-	    ConverterPlayer cp = new ConverterPlayer();
-	    PlayerDAO pdao = new PlayerDAO();
-	    PlayerEntity player;
-	    FullPlayer pl;	    
-	    for (int i = 0; i < pg.getItems().length; i++) {
-	    		
-	    	pl = pg.getItems()[i];    		
-	    		
-	    	if (pl.getPlayerType().equals("rare") || 
-	    		pl.getPlayerType().equals("standard")) {
-	    	
-	    		player = cp.FullToDB(pl);
-	    		pdao.Save(player);
-	    	}	    		
-	    		
-	    }
-		
-	}
+
+		}
+
+		LeagueController lgCtrl = new LeagueController();
+		lgCtrl.saveLeagues(leagues);
+
+	}	
 }
